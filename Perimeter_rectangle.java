d868cfc4211489f6341820dac35dd88b4a252853
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-01 12:55:30 +0530

    Add files via upload

---
+++ Perimeter_rectangle.java
@@ -0,0 +1,10 @@
+
+public class Perimeter_rectangle {
+    public static void main(String args[])
+    {
+        int l=5;
+        int b =7;
+        System.out.println("Perimeter= "+(2*(l+b)));
+        System.out.println("Area= "+(l*b));
+    }
+}
