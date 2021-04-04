d130d71d6de046146783978f6e7edcf0eddc8038
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-04 23:01:23 +0530

    Add files via upload

---
+++ Modulus.java
@@ -0,0 +1,8 @@
+public class Modulus {
+    public static void main(String[] args) {
+        int num=2345;
+        int n  =(num+8)/3;
+        int m =(n%5)*5;
+        System.out.println("Result is: "+m);
+    }
+}
