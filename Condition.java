e15cc29a268d2f5011e9b44d7844c4e6f4f6ba87
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-04 22:56:29 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,13 @@
+public class Condition {
+    public static void main(String[] args) {
+        int a=55;
+        int b =70;
+        if(a<50 && a<b)
+        
+            System.out.println("Both conditions are true");
+        else 
+            System.out.println("Either one of the condition is false or both are false");
+               
+    }
+    
+}
---
+++ Condition1.java
@@ -0,0 +1,12 @@
+public class Condition1 {
+    public static void main(String[] args) {
+        int a=55;
+        int b =70;
+        if(a<50 || a<b)
+        
+            System.out.println("Atleast one of the condition is true");
+        else 
+            System.out.println("Both are false");
+               
+    }
+}
