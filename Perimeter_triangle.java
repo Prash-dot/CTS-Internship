0fb8c68169780b512737c51d43fb5f945ee216ed
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-04 22:58:14 +0530

    Add files via upload

---
+++ Perimeter_triangle.java
@@ -0,0 +1,8 @@
+public class Perimeter_triangle {
+    public static void main(String[] args) {
+        int a=2;
+        int b =3;
+        int c =5;
+        System.out.println("Perimeter of trisngle =" +(a+b+c)+" units");
+    }
+}
