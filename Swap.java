d4ca75509f5c38a6afecb0985c834a12ca3d2afc
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-04 23:14:18 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a =6;
+        int b =8;
+        int temp =a;
+        a=b;
+        b=temp;
+        System.out.println("After swap:");
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+    }
+}
