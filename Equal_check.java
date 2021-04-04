3ec3d78f0f0bb899f05894aef6da7c303f216585
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-04 23:06:39 +0530

    Add files via upload

---
+++ Equal_check.java
@@ -0,0 +1,9 @@
+public class Equal_check {
+    public static void main(String[] args) {
+        int a=23, b=45;
+        if(a==b)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
