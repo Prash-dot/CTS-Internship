cdb809c2144ea423734ec9905fd4385f0e475cb6
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-05 09:20:56 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,9 @@
+public class product {
+    public static void main(String args[])
+    {
+        int a=8;
+        int b=2;
+        int c=6;
+        System.out.println("Product="+(a*b*c));
+    }
+}
