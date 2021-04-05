9778d0727d1858178d5987bf94df0e317347d0a3
Prash-dot <68656169+Prash-dot@users.noreply.github.com>
2021-04-05 09:18:31 +0530

    Add files via upload

---
+++ Temperture.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Temperture {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the temperature in fahrenheit");
+        double fahrenheit=sc.nextDouble();
+        double celsius=((fahrenheit-32)*5)/9;
+        System.out.println("Temperature in celsius = "+celsius);
+    }
+}
